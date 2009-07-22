4a2ae994d77aef675ca36c86152667695cb9c2b8
hadley <h.wickham@gmail.com>
2009-07-22 09:00:53 -0500

    Don't use mutatr object for ancestor iterator so it can be used with mutatr

--- ancestors.r
+++ ancestors.r
@@ -1,64 +1,62 @@
-source("object-inheritance.r")
-
-Ancestors <- Object$clone()$do({
-  self$init <- function(proto) {
-    self$todo <- list(proto)
-    self$done <- list()
+ancestor_iterator <- function(proto) {
+  todo <- list(proto)
+  done <- list()
+  
+  has_next <- function() {
+    length(todo) > 0
   }
   
-  self$has_next <- function() {
-    length(self$todo) > 0
-  }
-  
-  self$get_next <- function() {
-    if (!self$has_next()) return(NULL)
+  get_next <- function() {
+    if (!has_next()) return(NULL)
     
-    nxt <- self$pop()
+    nxt <- pop()
     
-    self$done <- c(self$done, list(nxt))
-    self$todo <- c(nxt$protos, self$todo)
-    self$remove_seen()
+    done <<- c(done, list(nxt))
+    todo <<- c(nxt$protos, todo)
+    remove_seen()
     
     nxt
   }
   
-  self$remove_seen <- function() {
-    if (length(self$todo) == 0) return()
-    seen <- unlist(lapply(self$todo, self$seen))
-    self$todo <- self$todo[!seen]
+  remove_seen <- function() {
+    if (length(todo) == 0) return()
+    seen <- unlist(lapply(todo, seen))
+    todo <<- todo[!seen]
   }
   
-  self$seen <- function(proto) {
-    any(sapply(self$done, identical, proto))
+  seen <- function(proto) {
+    any(sapply(done, identical, proto))
   }
   
-  self$pop <- function() {
-    pop <- self$todo[[1]]
-    self$todo <- self$todo[-1]
+  pop <- function() {
+    pop <- todo[[1]]
+    todo <<- todo[-1]
     pop
   }
-})
-
-Object$name <- "Object"
-a <- Object$clone()$do({self$name <- "a"})
-b <- a$clone()$do({self$name <- "b"})
-c <- b$clone()$do({self$name <- "c"})
-
-i <- Ancestors$clone(c)
-while(i$has_next()) {
-  print(i$get_next()$name)
+  
+  list(has_next = has_next, get_next = get_next)
 }
 
-d <- Object$clone()$do({self$name <- "d"})
-e <- Object$clone()$do({self$name <- "e"})
-e$append_proto(d)
-d$append_proto(e)
-
-i <- Ancestors$clone(d)
-while(i$has_next()) {
-  print(i$get_next()$name)
-}
-i <- Ancestors$clone(e)
-while(i$has_next()) {
-  print(i$get_next()$name)
-}
+# Object$name <- "Object"
+# a <- Object$clone()$do({self$name <- "a"})
+# b <- a$clone()$do({self$name <- "b"})
+# c <- b$clone()$do({self$name <- "c"})
+# 
+# i <- ancestor_iterator(c)
+# while(i$has_next()) {
+#   print(i$get_next()$name)
+# }
+# 
+# d <- Object$clone()$do({self$name <- "d"})
+# e <- Object$clone()$do({self$name <- "e"})
+# e$append_proto(d)
+# d$append_proto(e)
+# 
+# i <- ancestor_iterator(d)
+# while(i$has_next()) {
+#   print(i$get_next()$name)
+# }
+# i <- ancestor_iterator(e)
+# while(i$has_next()) {
+#   print(i$get_next()$name)
+# }
