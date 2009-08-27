67b7b790f7bec01e2b0f273db352bf42ae962d28
hadley <h.wickham@gmail.com>
2009-08-27 13:54:44 -0500

    Start of testing framework

--- tests/test-ancestors.r
+++ tests/test-ancestors.r
@@ -1,6 +1,32 @@
-source("assert.r")
+# Stateful interface, based around how tests are typically written in R 
+# already - interface shouldn't look object oriented because that's what it
+# normally is.  
+#
+# When run stand-alone (without global suite object), each just immediately
+# returns results (as everything should work well interactively).  When run
+# as part of a test suite, each assertion contributes to the global object, 
+# which produces a final report.
+# 
+# should_be_error
+# should_be_identical
+# should_be_true
+# 
+# 
+# should_be_same_as_last_time
+#   default lookup default should hash of deparse + current test context
+#
+# Ancestor iteration:
+#   Simple hierarchical case: ...F..
+#   * a not identical to b
+# 
+#   Circular case: ...
+#
+test_context <- test <- function(...) {}
 
-# Test simple hierarchical case
+test_context("Ancestor iterator")
+
+# ----------------------------------------------------------------------------
+test("Simple hierarchical case")  
 a <- Object$clone()
 b <- a$clone()
 c <- b$clone()
@@ -14,7 +40,9 @@ assert(c$has_ancestor(a))
 assert(c$has_ancestor(Object))
 assert(!c$has_ancestor(c))
 
-# Test circular case
+
+# ----------------------------------------------------------------------------
+test("Circular case")
 d <- Object$clone()
 e <- Object$clone()
 e$append_proto(d)
