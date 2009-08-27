dd8b5e03f1ddd09a4b42ba24a73dd02d1f76068d
hadley <h.wickham@gmail.com>
2009-08-27 10:48:10 -0500

    Test based on object equality, not object name

--- tests/ancestors.r
+++ tests/ancestors.r
@@ -2,32 +2,31 @@ all_ancestors <- function(obj) {
   i <- ancestor_iterator(obj)
   i$get_next() # Skip self
   
-  ancestors <- c()
+  ancestors <- list()
   while(i$has_next()) {
-    ancestors <- c(ancestors, i$get_next()$name)
+    ancestors <- c(ancestors, list(i$get_next()))
   }
   ancestors
 }
 
-assert_has_ancestors <- function(obj, ancestors) {
+assert_ancestors_are <- function(obj, ancestors) {
   stopifnot(identical(all_ancestors(obj), ancestors))
 }
 
 # Test simple hierarchical case
-Object$name <- "Object"
-a <- Object$clone()$do({self$name <- "a"})
-b <- a$clone()$do({self$name <- "b"})
-c <- b$clone()$do({self$name <- "c"})
+a <- Object$clone()
+b <- a$clone()
+c <- b$clone()
 
-assert_has_ancestors(a, "Object")
-assert_has_ancestors(b, c("a", "Object"))
-assert_has_ancestors(c, c("b", "a", "Object"))
+assert_ancestors_are(a, list(Object))
+assert_ancestors_are(b, list(a, Object))
+assert_ancestors_are(c, list(b, a, Object))
 
 # Test circular case
-d <- Object$clone()$do({self$name <- "d"})
-e <- Object$clone()$do({self$name <- "e"})
+d <- Object$clone()
+e <- Object$clone()
 e$append_proto(d)
 d$append_proto(e)
 
-assert_has_ancestors(e, c("Object", "d"))
-assert_has_ancestors(d, c("Object", "e"))
+assert_ancestors_are(e, list(Object, d))
+assert_ancestors_are(d, list(Object, e))
