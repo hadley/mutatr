d00abaffea808f55418217194a404929e576db2d
hadley <h.wickham@gmail.com>
2009-09-03 22:41:43 -0500

    Remove testing code now located in test_that

--- tests/run_all.r
+++
@@ -1,9 +0,0 @@
-frame_files <- Filter(Negate(is.null), lapply(sys.frames(), function(x) x$ofile))
-FILE <- frame_files[[length(frame_files)]]
-
-PATH <- dirname(FILE)
-
-source(file.path(PATH, "assert.r"))
-tests <- setdiff(dir(PATH, "test-", full.names = TRUE), FILE)
-
-lapply(tests, source, chdir = TRUE)
